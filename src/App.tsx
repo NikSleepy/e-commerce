97939e79ff57524480f257b79435cc715b89f566
NikSleepy <nikriaria@gmail.com>
2024-05-21 23:45:28 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/Home";
-import Login from "./pages/login";
-import Register from "./pages/register";
-import Layout from "./layout";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/checkout";
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Login from './pages/login';
+import Register from './pages/register';
+import Layout from './layout';
+import Cart from './pages/Cart';
+import Checkout from './pages/checkout';
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/checkout" element={<Checkout />} />
-                </Route>
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -10,29 +10,29 @@ import {
   Th,
   Thead,
   Tr,
-} from "@chakra-ui/react";
-import React from "react";
-import data from "../../mock/databestselling.json";
-import { InputQuantity } from "./Component/InputQuantity";
-import { useNavigate } from "react-router-dom";
+} from '@chakra-ui/react';
+import React from 'react';
+import data from '../../mock/databestselling.json';
+import { InputQuantity } from './Component/InputQuantity';
+import { useNavigate } from 'react-router-dom';
 const Component: React.FC = () => {
   const navigate = useNavigate();
   return (
-    <Box w={"100%"} display={"grid"} justifyItems={"center"} gap={"10px"}>
+    <Box w={'100%'} display={'grid'} justifyItems={'center'} gap={'10px'}>
       <Box
-        display={"flex"}
-        color={"#C1C0C1"}
-        fontSize={"15px"}
-        gap={"20px"}
-        w={"80%"}
-        my={"30px"}
+        display={'flex'}
+        color={'#C1C0C1'}
+        fontSize={'15px'}
+        gap={'20px'}
+        w={'80%'}
+        my={'30px'}
       >
         <Text>Home</Text>
         <Text> / </Text>
-        <Text color={"black"}> Cart</Text>
+        <Text color={'black'}> Cart</Text>
       </Box>
 
-      <TableContainer w={"80%"}>
+      <TableContainer w={'80%'}>
         <Table>
           <Thead>
             <Tr>
@@ -51,75 +51,75 @@ const Component: React.FC = () => {
       </TableContainer>
 
       <Box
-        display={"flex"}
-        justifyContent={"space-between"}
-        w={"80%"}
-        my={"30px"}
+        display={'flex'}
+        justifyContent={'space-between'}
+        w={'80%'}
+        my={'30px'}
       >
         <Button
-          color={"white"}
-          bg={"#DB4444"}
-          border={"1px"}
-          _hover={{ bg: "black", color: "white" }}
+          color={'white'}
+          bg={'#DB4444'}
+          border={'1px'}
+          _hover={{ bg: 'black', color: 'white' }}
         >
           Return to Shop
         </Button>
         <Button
-          color={"white"}
-          bg={"#DB4444"}
-          border={"1px"}
-          _hover={{ bg: "black", color: "white" }}
+          color={'white'}
+          bg={'#DB4444'}
+          border={'1px'}
+          _hover={{ bg: 'black', color: 'white' }}
         >
           Update Cart
         </Button>
       </Box>
 
       <Box
-        w={"80%"}
-        display={"flex"}
-        justifyContent={"space-between"}
-        mt={"30px"}
-        mb={"70px"}
+        w={'80%'}
+        display={'flex'}
+        justifyContent={'space-between'}
+        mt={'30px'}
+        mb={'70px'}
       >
-        <Flex gap={"10px"}>
-          <Input placeholder="Enter your coupon code" border={'1px'}/>
+        <Flex gap={'10px'}>
+          <Input placeholder="Enter your coupon code" border={'1px'} />
           <Button
-            color={"white"}
-            bg={"#DB4444"}
-            border={"1px"}
-            _hover={{ bg: "black", color: "white" }}
+            color={'white'}
+            bg={'#DB4444'}
+            border={'1px'}
+            _hover={{ bg: 'black', color: 'white' }}
           >
             Apply
           </Button>
         </Flex>
         <Box
-          w={"35%"}
-          display={"grid"}
-          gap={"20px"}
-          border={"1px"}
-          rounded={"md"}
-          p={"15px"}
+          w={'35%'}
+          display={'grid'}
+          gap={'20px'}
+          border={'1px'}
+          rounded={'md'}
+          p={'15px'}
         >
           <Text>Total</Text>
-          <Flex justifyContent={"space-between"} borderBottom={"1px"}>
+          <Flex justifyContent={'space-between'} borderBottom={'1px'}>
             <Text>SubTotal: </Text>
             <Text>Rp.1000.000</Text>
           </Flex>
-          <Flex justifyContent={"space-between"} borderBottom={"1px"}>
+          <Flex justifyContent={'space-between'} borderBottom={'1px'}>
             <Text>Shipping: </Text>
             <Text>Free</Text>
           </Flex>
-          <Flex justifyContent={"space-between"}>
+          <Flex justifyContent={'space-between'}>
             <Text>Total: </Text>
             <Text>Rp. 10.000.000</Text>
           </Flex>
 
           <Button
-            color={"white"}
-            bg={"#DB4444"}
-            border={"1px"}
-            _hover={{ bg: "black", color: "white" }}
-            onClick={() => navigate("/checkout")}
+            color={'white'}
+            bg={'#DB4444'}
+            border={'1px'}
+            _hover={{ bg: 'black', color: 'white' }}
+            onClick={() => navigate('/checkout')}
           >
             Checkout
           </Button>
